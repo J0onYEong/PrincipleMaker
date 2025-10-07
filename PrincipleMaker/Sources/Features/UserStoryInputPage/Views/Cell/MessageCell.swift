77dec5893454dd43d4b92fdc6e2575f6e4f5e3ec
J0onYEong <cjy2134@naver.com>
2025-10-07 22:06:55 +0900

    Feat, 메세지 셀 구현

---
+++ PrincipleMaker/Sources/Features/UserStoryInputPage/Views/Cell/MessageCell.swift
@@ -0,0 +1,114 @@
+//
+//  MessageCell.swift
+//  PrincipleMaker
+//
+//  Created by choijunios on 10/7/25.
+//
+
+import SnapKit
+import UIKit
+
+enum StoryMessageMode {
+    case message(String)
+    case typing
+}
+
+final class MessageCell: UITableViewCell {
+    private enum Config {
+        static let messageLabelInset: CGFloat = 10
+        static let messageContainerMinWidth: CGFloat = 50
+        static let messageContainerMinHeight: CGFloat = 30
+    }
+    private let hostImageView: UIImageView = UIImageView()
+    private let messageContainer: UIView = UIView()
+    private let messageLabel: UILabel = UILabel()
+    private let loadingImage: UIImageView = UIImageView()
+    
+    // Animation
+    private var loadingTimer: Timer?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        attribute()
+        layout()
+    }
+    required init?(coder: NSCoder) { nil }
+    
+    override func prepareForReuse() {
+        stopLoadingAnimation()
+    }
+    
+    private func attribute() {
+        hostImageView.image = UIImage(systemName: "person.circle")
+        hostImageView.tintColor = .systemGreen
+        hostImageView.layer.cornerRadius = 10
+        
+        messageContainer.layer.cornerRadius = 10
+        messageContainer.backgroundColor = .lightGray
+        
+        messageLabel.font = .systemFont(ofSize: 17)
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0
+        
+        loadingImage.image = UIImage(systemName: "hourglass")
+        loadingImage.tintColor = .white
+    }
+    
+    private func layout() {
+        contentView.addSubview(hostImageView)
+        hostImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.equalToSuperview()
+            make.width.height.equalTo(30)
+        }
+        
+        contentView.addSubview(messageContainer)
+        messageContainer.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(15)
+            make.left.equalTo(hostImageView.snp.right).offset(3)
+            make.right.lessThanOrEqualToSuperview().inset(20)
+            
+            make.height.greaterThanOrEqualTo(Config.messageContainerMinHeight)
+            make.width.greaterThanOrEqualTo(Config.messageContainerMinWidth)
+        }
+        
+        messageContainer.addSubview(messageLabel)
+        messageLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(Config.messageLabelInset)
+            make.verticalEdges.equalToSuperview().inset(Config.messageLabelInset)
+        }
+        
+        messageContainer.addSubview(loadingImage)
+        loadingImage.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(15)
+        }
+    }
+    
+    private func startLoadingAnimation() {
+        loadingImage.addSymbolEffect(.rotate, options: .repeat(.periodic))
+    }
+    
+    private func stopLoadingAnimation() {
+        loadingImage.removeAllSymbolEffects()
+    }
+    
+    func configure(using mode: StoryMessageMode) {
+        switch mode {
+        case .message(let string):
+            loadingImage.isHidden = true
+            stopLoadingAnimation()
+            messageLabel.text = string
+        case .typing:
+            loadingImage.isHidden = false
+            startLoadingAnimation()
+            messageLabel.text = ""
+        }
+    }
+}
+
+#Preview(traits: .defaultLayout) {
+    let view = MessageCell()
+    view.configure(using: .message("HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"))
+    return view
+}
