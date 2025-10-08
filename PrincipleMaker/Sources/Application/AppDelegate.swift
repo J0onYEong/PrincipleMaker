89037e19005b90a0c8c6e9e2bb6201a377618c90
J0onYEong <cjy2134@naver.com>
2025-10-08 21:20:32 +0900

    Feat, 의존성 주입 위치 수정

--- PrincipleMaker/Sources/Application/AppDelegate.swift
+++ PrincipleMaker/Sources/Application/AppDelegate.swift
@@ -4,11 +4,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        executeDependencyInjection()
+        
         return true
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    private func executeDependencyInjection() {
+        DependencyInjector.shared.register(
+            UserStoryDialogProvider.self,
+            DefaultUserStoryDialogProvider()
+        )
     }
 }
 
--- PrincipleMaker/Sources/Application/SceneDelegate.swift
+++ PrincipleMaker/Sources/Application/SceneDelegate.swift
@@ -5,14 +5,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
-        
-        executeDependencyInjection()
         
         let viewController = UserStoryInputViewController()
         viewController.bind(viewModel: UserStoryInputViewModel())
@@ -21,13 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             rootViewController: viewController
         )
         window.makeKeyAndVisible()
-    }
-    
-    private func executeDependencyInjection() {
-        DependencyInjector.shared.register(
-            UserStoryDialogProvider.self,
-            DefaultUserStoryDialogProvider()
-        )
     }
 }
 
